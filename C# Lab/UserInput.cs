2dc9299e90ef5f1eb35ffd939eedee5f161bf260
jtn9701 <jtnguy01@gmail.com>
2026-07-27 14:34:30 -0500

    Implement class that uses user input

---
+++ C# Lab/UserInput.cs
@@ -0,0 +1,42 @@
+﻿class UserInput
+{
+    public static string GetUserName()
+    {
+        string? userName;
+
+        do
+        {
+            Console.WriteLine("What is your name?");
+            userName = Console.ReadLine();
+        }
+        while (string.IsNullOrWhiteSpace(userName));
+
+        return userName.Trim();
+            
+    }
+
+    public static int GetUserAge()
+    {
+        int userAge;
+        bool isValid;
+
+        do
+        {
+            Console.WriteLine("What is your age?");
+            isValid = int.TryParse(Console.ReadLine(), out userAge) && userAge is >= 0 and <= 120;
+
+            if (!isValid)
+            {
+                Console.WriteLine("Please enter a valid age");
+            }
+
+        } while (!isValid);
+
+        return userAge;
+    }
+
+    public static void WriteGreeting(string name, int age)
+    {
+        Console.WriteLine($"Hi, {name}! Your age is {age}.");
+    }
+}
